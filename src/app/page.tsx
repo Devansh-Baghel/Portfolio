ed69b466a13ef2fe13495fefd193f113c05645aa
Devansh Baghel <77718741+Devansh-Baghel@users.noreply.github.com>
2024-04-05 18:05:54 +0530

    style: ui changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 export default function Home() {
   return (
-    <div className="w-screen h-screen bg-white text-3xl hero">
-      <img
-        src="/shape-76.svg"
-        alt=""
-        className="images fixed animate-spin animate-infinite animate-duration-[20000ms] animate-ease-in-out w-[400px] h-[400px] top-[-120px] left-[-120px]"
-      />
-      <div className="fixed">
-        <h1 className="text-[3.5em] absolute top-10 left-10 text-slate-900 leading-tight">
+    <main className="w-screen h-screen bg-white text-3xl hero max-w-[1400px] relative m-auto">
+      <aside className="fixed text-slate-900 max-w-[600px]">
+        <img
+          src="/shape-76.svg"
+          alt=""
+          className="images absolute animate-spin animate-infinite animate-duration-[20000ms] animate-ease-in-out w-[400px] h-[400px] top-[-120px] left-[-80px]"
+        />
+        <h1 className="text-[3.5em] absolute left-10 top-10 leading-tight">
           Devansh Baghel
           {/* baghel.dev */}
           {/* <span className="box">a</span>
@@ -16,7 +16,17 @@ export default function Home() {
           <span className="box">a</span>
           <span className="box">a</span> */}
         </h1>
-      </div>
+        <h3 className="mt-[350px] ml-[50px] text-[38px] text-slate-800">
+          Fullstack Developer
+        </h3>
+        <p className="ml-[50px] text-2xl mt-4">
+          Hello there 👋, I'm Devansh Baghel and I build fullstack web apps with{" "}
+          <span className="underline  cursor-pointer underline-offset-4">
+            modern tools
+          </span>
+          .
+        </p>
+      </aside>
       {/* <div className="fixed bg-white right-0 top-0 h-20 w-[700px] z-10"></div> */}
       {/* <div className="fixed bg-white right-0 bottom-0 h-20 w-[700px] z-10"></div> */}
       <div className="w-[700px] h-[500px] absolute right-0 z-0 images">
@@ -25,6 +35,6 @@ export default function Home() {
         <img src="/Grad_10.png" alt="" />
         <img src="/Grad_05.png" alt="" />
       </div>
-    </div>
+    </main>
   );
 }

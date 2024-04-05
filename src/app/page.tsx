a1deba6b56e6a0bc4e49130c6d8cfa4fa2d51779
Devansh Baghel <77718741+Devansh-Baghel@users.noreply.github.com>
2024-04-05 20:50:00 +0530

    ui: add buttons + contact links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
+import { FaGithub as GithubIcon } from "react-icons/fa";
+import { FaLinkedin as LinkedinIcon } from "react-icons/fa";
+import { MdOutgoingMail as MailIcon } from "react-icons/md";
+
 export default function Home() {
   return (
     <main className="w-screen h-screen bg-white text-3xl hero max-w-[1400px] relative m-auto">
@@ -9,26 +13,36 @@ export default function Home() {
         />
         <h1 className="text-[3.5em] absolute left-10 top-10 leading-tight">
           Devansh Baghel
-          {/* baghel.dev */}
-          {/* <span className="box">a</span>
-          <span className="box">a</span>
-          <span className="box">a</span>
-          <span className="box">a</span>
-          <span className="box">a</span> */}
         </h1>
         <h3 className="mt-[350px] ml-[50px] text-[38px] text-slate-800">
           Fullstack Developer
         </h3>
         <p className="ml-[50px] text-2xl mt-4">
           Hello there 👋, I'm Devansh Baghel and I build fullstack web apps with{" "}
-          <span className="underline  cursor-pointer underline-offset-4">
+          <span className="underline cursor-pointer underline-offset-2">
             modern tools
           </span>
           .
         </p>
+        <button className="text-xl px-6 py-2 border-[3px] font-medium border-slate-900 rounded-[30px] mt-6 ml-[50px] bg-slate-900 text-white">
+          Résumé
+        </button>
+        <button className="text-xl px-6 py-2 border-[3px] font-medium border-slate-900 rounded-[30px] mt-4 ml-[20px] ">
+          Contact me
+        </button>
+
+        <div className="mt-6 ml-[50px] flex gap-4 items-center">
+          <a href="https://github.com/devansh-baghel" target="_blank">
+            <GithubIcon className="w-8 h-8" />
+          </a>
+          <a href="https://linkedin.com/in/devanshbaghel" target="_blank">
+            <LinkedinIcon className="w-8 h-8" />
+          </a>
+          <a href="mailto:hello@baghel.dev" target="_blank">
+            <MailIcon className="w-10 h-10" />
+          </a>
+        </div>
       </aside>
-      {/* <div className="fixed bg-white right-0 top-0 h-20 w-[700px] z-10"></div> */}
-      {/* <div className="fixed bg-white right-0 bottom-0 h-20 w-[700px] z-10"></div> */}
       <div className="w-[700px] h-[500px] absolute right-0 z-0 images">
         <img src="/Grad_07.png" alt="" />
         <img src="/Grad_02.png" alt="" />

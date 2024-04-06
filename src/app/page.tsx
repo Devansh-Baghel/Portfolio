ae3a89681f58eddc027750d464f2577deb90f488
Devansh Baghel <77718741+Devansh-Baghel@users.noreply.github.com>
2024-04-06 22:23:55 +0530

    ui: tech-stack basic layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,9 +22,12 @@ export default function Home() {
         <p className="ml-[50px] text-2xl mt-4">
           Hello there 👋, I'm Devansh Baghel, and I build full-stack web apps
           with{" "}
-          <span className="underline cursor-pointer underline-offset-2">
+          <a
+            href="#tech-stack"
+            className="underline cursor-pointer underline-offset-2"
+          >
             modern tools
-          </span>
+          </a>
           .
         </p>
         <button className="text-xl px-6 py-2 border-[3px] font-medium border-slate-900 rounded-[30px] mt-6 ml-[50px] bg-slate-900 text-white">
@@ -53,7 +56,10 @@ export default function Home() {
         <img src="/Grad_05.png" alt="" />
       </div>
 
-      <section className="absolute right-12 top-[100vh] flex flex-col gap-6">
+      <section
+        className="absolute right-12 top-[100vh] flex flex-col gap-6"
+        id="projects"
+      >
         <h3 className="text-4xl mb-4 text-slate-900">Projects</h3>
 
         <div className="wrapper p-6 border-slate-900 border-[3px] rounded-[30px] flex flex-col gap-4">
@@ -133,6 +139,102 @@ export default function Home() {
         >
           More on github
         </a>
+      </section>
+
+      <section
+        className="absolute right-12 top-[200vh] flex flex-col gap-6 pt-10"
+        id="tech-stack"
+      >
+        <h3 className="text-4xl mb-4 text-slate-900">Tech Stack / Tools</h3>
+
+        <div className="wrapper p-6 border-slate-900 border-[3px] rounded-[30px] flex flex-col gap-4">
+          <h3 className="text-3xl text-slate-900">Frontend</h3>
+
+          <div className="badge-container">
+            <img
+              src="https://img.shields.io/badge/typescript-%23007ACC.svg?style=for-the-badge&amp;logo=typescript&amp;logoColor=white"
+              alt="TypeScript"
+            />
+            <img
+              src="https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&amp;logo=javascript&amp;logoColor=%23F7DF1E"
+              alt="JavaScript"
+            />
+            <img
+              src="https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&amp;logo=react&amp;logoColor=%2361DAFB"
+              alt="React"
+            />
+            <img
+              src="https://img.shields.io/badge/-React%20Query-FF4154?style=for-the-badge&amp;logo=react%20query&amp;logoColor=white"
+              alt="React Query"
+            />
+            <img
+              src="https://img.shields.io/badge/React_Router-CA4245?style=for-the-badge&amp;logo=react-router&amp;logoColor=white"
+              alt="React Router"
+            />
+            <img
+              src="https://img.shields.io/badge/Context--Api-000000?style=for-the-badge&amp;logo=react"
+              alt="Context-API"
+            />
+            <img
+              src="https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&amp;logo=html5&amp;logoColor=white"
+              alt="HTML5"
+            />
+            <img
+              src="https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&amp;logo=css3&amp;logoColor=white"
+              alt="CSS3"
+            />
+            <img
+              src="https://img.shields.io/badge/tailwindcss-%2338B2AC.svg?style=for-the-badge&amp;logo=tailwind-css&amp;logoColor=white"
+              alt="TailwindCSS"
+            />
+          </div>
+        </div>
+        <div className="wrapper p-6 border-slate-900 border-[3px] rounded-[30px]">
+          <h3 className="text-3xl text-slate-900">Backend</h3>
+
+          <div className="badge-container">
+            <img
+              src="https://img.shields.io/badge/Next-black?style=for-the-badge&amp;logo=next.js&amp;logoColor=white"
+              alt="Next JS"
+            />
+            <img
+              src="https://img.shields.io/badge/node.js-6DA55F?style=for-the-badge&amp;logo=node.js&amp;logoColor=white"
+              alt="NodeJS"
+            />
+            <img
+              src="https://img.shields.io/badge/express.js-%23404d59.svg?style=for-the-badge&amp;logo=express&amp;logoColor=%2361DAFB"
+              alt="Express.js"
+            />
+            <img
+              src="https://img.shields.io/badge/MongoDB-%234ea94b.svg?style=for-the-badge&amp;logo=mongodb&amp;logoColor=white"
+              alt="MongoDB"
+            />
+            <img
+              src="https://img.shields.io/badge/JWT-black?style=for-the-badge&amp;logo=JSON%20web%20tokens"
+              alt="JWT"
+            />
+            <img
+              src="https://img.shields.io/badge/Bun-%23000000.svg?style=for-the-badge&amp;logo=bun&amp;logoColor=white"
+              alt="Bun"
+            />
+            <img
+              src="https://img.shields.io/badge/deno%20js-000000?style=for-the-badge&amp;logo=deno&amp;logoColor=white"
+              alt="Deno JS"
+            />
+            <img
+              src="https://img.shields.io/badge/docker-%230db7ed.svg?style=for-the-badge&amp;logo=docker&amp;logoColor=white"
+              alt="Docker"
+            />
+
+            <img
+              src="https://img.shields.io/badge/Postman-FF6C37?style=for-the-badge&amp;logo=postman&amp;logoColor=white"
+              alt="Postman"
+            />
+          </div>
+        </div>
+        <div className="wrapper p-6 h-24 border-slate-900 border-[3px] rounded-[30px]">
+          <h3 className="text-3xl text-slate-900">Miscellaneous</h3>
+        </div>
       </section>
     </main>
   );

0085ced73afab5dbd9b7b17c31db6e51e4f9026a
Devansh Baghel <77718741+Devansh-Baghel@users.noreply.github.com>
2024-04-07 15:08:24 +0530

    responsive main page

--- src/app/api/email/route.ts
+++ src/app/api/email/route.ts
@@ -20,7 +20,7 @@ export async function POST(request: NextRequest) {
       );
 
     const data = await resend.emails.send({
-      from: "Contact <onboarding@resend.dev>",
+      from: "Portfolio Contact <contact@baghel.dev>",
       to: ["devanshbaghel85@gmail.com"],
       subject: "Someone Contacted You",
       react: EmailTemplate({ name, message }),
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,19 +7,19 @@ import TechStack from "./components/Stack";
 export default function Home() {
   return (
     <main className="w-screen h-screen bg-white text-3xl hero max-w-[1400px] relative m-auto">
-      <aside className="fixed text-slate-900 max-w-[600px]">
+      <aside className="md:fixed text-slate-900 max-w-[600px]">
         <img
           src="/shape-76.svg"
           alt=""
           className="images absolute animate-spin animate-infinite animate-duration-[40000ms] animate-ease-in-out w-[400px] h-[400px] top-[-120px] left-[-80px]"
         />
-        <h1 className="text-[3.5em] absolute left-10 top-10 leading-tight">
+        <h1 className="text-[2em] relative z-10 md:text-[3.5em] md:absolute left-6 md:top-10 top-10 leading-tight">
           Devansh Baghel
         </h1>
-        <h3 className="mt-[350px] ml-[50px] text-[38px] text-slate-800">
+        <h3 className="md:mt-[350px] relative z-10 mt-28 ml-6 sm:ml-10 md:ml-[50px] text-[0.9em] md:text-[38px] text-slate-800">
           Fullstack Developer
         </h3>
-        <p className="ml-[50px] text-2xl mt-4">
+        <p className="md:ml-[50px] relative z-10 ml-6 sm:ml-10 text-xl mt-4">
           Hello there 👋, I'm Devansh Baghel, and I build full-stack web apps
           with{" "}
           <a
@@ -30,14 +30,14 @@ export default function Home() {
           </a>
           .
         </p>
-        <button className="text-xl px-6 py-2 border-[3px] font-medium border-slate-900 rounded-[30px] mt-6 ml-[50px] bg-slate-900 text-white">
+        <button className="text-sm md:text-xl px-6 py-2 border-[2px] font-medium border-slate-900 rounded-[30px] mt-6 md:ml-[50px] ml-6 sm:ml-10 bg-slate-900 text-white">
           Résumé
         </button>
-        <button className="text-xl px-6 py-2 border-[3px] font-medium border-slate-900 rounded-[30px] mt-4 ml-[20px] ">
+        <button className="text-sm md:text-xl px-6 py-2 border-[2px] font-medium border-slate-900 rounded-[30px] mt-4 ml-[20px] ">
           Contact me
         </button>
 
-        <div className="mt-6 ml-[50px] flex gap-4 items-center">
+        <div className="mt-6 md:ml-[50px] ml-6 sm:ml-10 flex gap-4 items-center">
           <a href="https://github.com/devansh-baghel" target="_blank">
             <GithubIcon className="w-8 h-8" />
           </a>
@@ -49,8 +49,8 @@ export default function Home() {
           </a>
         </div>
       </aside>
-      <div className="w-[700px] h-[500px] absolute right-0 z-0 images">
-        <img src="/Grad_07.png" alt="" />
+      <div className="w-[700px] h-[500px] absolute right-0 z-0 images top-[100vh] md:top-0">
+        <img src="/Grad_07.png" alt="" className="hidden md:block" />
         <img src="/Grad_02.png" alt="" />
         <img src="/Grad_10.png" alt="" className="mt-[10vh]" />
         <img src="/Grad_05.png" alt="" className="mt-[10vh]" />

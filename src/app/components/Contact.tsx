1c8ac15ea91b11aed73423e8896a59ab285258c7
Devansh Baghel <77718741+Devansh-Baghel@users.noreply.github.com>
2024-05-06 17:46:07 +0530

    phone: contact page

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -15,16 +15,18 @@ export default function Contact() {
   }
 
   return (
-    <section id="contact" className="text-slate-900 p-6">
-      <h3>Contact me</h3>
-      <form action={sendEmail}>
+    <section id="contact" className="text-slate-900 p-4 flex flex-col gap-6 mt-20 pb-20">
+      <h3 className="text-4xl">Contact me</h3>
+      <form action={sendEmail} className="flex flex-col gap-6 text-xl placeholder:text-xl">
         <input
+          className="wrapper p-6 md:h-20 border-slate-900 border-[3px] rounded-[30px] md:w-[600px] placeholder:text-xl placeholder:text-slate-800"
           required
           placeholder="Your name"
           id="name"
           onChange={(e) => setName(e.target.value)}
         />
         <input
+          className="wrapper p-6 md:h-20 border-slate-900 border-[3px] rounded-[30px] md:w-[600px] placeholder:text-xl placeholder:text-slate-800"
           required
           placeholder="Your email"
           id="email"
@@ -32,13 +34,16 @@ export default function Contact() {
           onChange={(e) => setEmail(e.target.value)}
         />
         <textarea
+          className="wrapper p-6 md:h-32 border-slate-900 border-[3px] rounded-[30px] md:w-[600px] placeholder:text-xl placeholder:text-slate-800"
           required
           placeholder="Your message"
           id="message"
           onChange={(e) => setMessage(e.target.value)}
         />
 
-        <button>Send</button>
+        <button className="text-sm md:text-xl px-6 py-4 border-[2px] font-medium border-slate-900 rounded-[30px] bg-slate-900 text-white">
+          Send
+        </button>
       </form>
     </section>
   );

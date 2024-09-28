1f2a27cbda4ac4abd2cb25981316998483f9e96c
Devansh Baghel <devanshbaghel85@gmail.com>
2024-09-28 15:03:53 +0530

    fix: remove comments

--- src/app/components/Stack.tsx
+++ src/app/components/Stack.tsx
@@ -37,50 +37,6 @@ export default function TechStack() {
       <div className="wrapper p-6 border-slate-900 border-[3px] rounded-[30px]">
         <h3 className="text-3xl mb-4">Frontend</h3>
 
-        {/* <div className="badge-container">
-          <img
-            src="https://img.shields.io/badge/Next-black?style=for-the-badge&amp;logo=next.js&amp;logoColor=white"
-            alt="Next JS"
-          />
-          <img
-            src="https://img.shields.io/badge/typescript-%23007ACC.svg?style=for-the-badge&amp;logo=typescript&amp;logoColor=white"
-            alt="TypeScript"
-          />
-          <img
-            src="https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&amp;logo=javascript&amp;logoColor=%23F7DF1E"
-            alt="JavaScript"
-          />
-          <img
-            src="https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&amp;logo=react&amp;logoColor=%2361DAFB"
-            alt="React"
-          />
-          <img
-            src="https://img.shields.io/badge/-React%20Query-FF4154?style=for-the-badge&amp;logo=react%20query&amp;logoColor=white"
-            alt="React Query"
-          />
-          <img
-            src="https://img.shields.io/badge/React_Router-CA4245?style=for-the-badge&amp;logo=react-router&amp;logoColor=white"
-            alt="React Router"
-          />
-          <img
-            src="https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&amp;logo=html5&amp;logoColor=white"
-            alt="HTML5"
-          />
-          <img
-            src="https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&amp;logo=css3&amp;logoColor=white"
-            alt="CSS3"
-          />
-          <img
-            src="https://img.shields.io/badge/tailwindcss-%2338B2AC.svg?style=for-the-badge&amp;logo=tailwind-css&amp;logoColor=white"
-            alt="TailwindCSS"
-          />
-        </div> */}
-        {/* <Image
-          height={100}
-          width={400}
-          alt="Frontend Tools"
-          src="https://go-skill-icons.vercel.app/api/icons?i=nextjs,ts,js,react,reactquery,tailwind,html,css"
-        /> */}
         <div className="flex flex-wrap gap-4">
           {frontend.map((elm) => (
             <Image
@@ -96,43 +52,6 @@ export default function TechStack() {
       <div className="wrapper p-6 border-slate-900 border-[3px] rounded-[30px]">
         <h3 className="text-3xl mb-4">Backend</h3>
 
-        {/* <div className="badge-container">
-          <img
-            src="https://img.shields.io/badge/node.js-6DA55F?style=for-the-badge&amp;logo=node.js&amp;logoColor=white"
-            alt="NodeJS"
-          />
-          <img
-            src="https://img.shields.io/badge/express.js-%23404d59.svg?style=for-the-badge&amp;logo=express&amp;logoColor=%2361DAFB"
-            alt="Express.js"
-          />
-          <img
-            src="https://img.shields.io/badge/MongoDB-%234ea94b.svg?style=for-the-badge&amp;logo=mongodb&amp;logoColor=white"
-            alt="MongoDB"
-          />
-          <img
-            src="https://img.shields.io/badge/JWT-black?style=for-the-badge&amp;logo=JSON%20web%20tokens"
-            alt="JWT"
-          />
-          <img
-            src="https://img.shields.io/badge/Postman-FF6C37?style=for-the-badge&amp;logo=postman&amp;logoColor=white"
-            alt="Postman"
-          />
-          <img
-            src="https://img.shields.io/badge/docker-%230db7ed.svg?style=for-the-badge&amp;logo=docker&amp;logoColor=white"
-            alt="Docker"
-          />
-          <img
-            src="https://img.shields.io/badge/vercel-%23000000.svg?style=for-the-badge&amp;logo=vercel&amp;logoColor=white"
-            alt="Vercel"
-          />
-        </div> */}
-        {/* <Image
-          // fill
-          height={50}
-          width={400}
-          alt="Backend Tools"
-          src="https://go-skill-icons.vercel.app/api/icons?i=mongodb,nodejs,express,postman,docker,heroku,python,asd"
-        /> */}
         <div className="flex flex-wrap gap-4">
           {backend.map((elm) => (
             <Image
@@ -148,45 +67,6 @@ export default function TechStack() {
       <div className="wrapper p-6 border-slate-900 border-[3px] rounded-[30px]">
         <h3 className="text-3xl mb-4">Miscellaneous</h3>
 
-        {/* <div className="badge-container">
-          <img
-            src="https://img.shields.io/badge/Linux-FCC624?style=for-the-badge&amp;logo=linux&amp;logoColor=black"
-            alt="Linux"
-          />
-          <img
-            src="https://img.shields.io/badge/git-%23F05033.svg?style=for-the-badge&amp;logo=git&amp;logoColor=white"
-            alt="Git"
-          />
-          <img
-            src="https://img.shields.io/badge/github-%23121011.svg?style=for-the-badge&amp;logo=github&amp;logoColor=white"
-            alt="GitHub"
-          />
-
-          <img
-            src="https://img.shields.io/badge/-GraphQL-E10098?style=for-the-badge&amp;logo=graphql&amp;logoColor=white"
-            alt="GraphQL"
-          />
-          <img
-            src="https://img.shields.io/badge/shell_script-%23121011.svg?style=for-the-badge&amp;logo=gnu-bash&amp;logoColor=white"
-            alt="Shell Script"
-          />
-          <img
-            src="https://img.shields.io/badge/python-3670A0?style=for-the-badge&amp;logo=python&amp;logoColor=ffdd54"
-            alt="Python"
-          />
-          <img
-            src="https://img.shields.io/badge/NeoVim-%2357A143.svg?&amp;style=for-the-badge&amp;logo=neovim&amp;logoColor=white"
-            alt="Neovim"
-          />
-        </div> */}
-
-        {/* <Image
-          // fill
-          height={80}
-          width={400}
-          alt="Miscellaneous Tools"
-          src="https://go-skill-icons.vercel.app/api/icons?i=linux,git,github,bash,neovim,asd,asd,asd"
-        /> */}
         <div className="flex flex-wrap gap-4">
           {misc.map((elm) => (
             <Image

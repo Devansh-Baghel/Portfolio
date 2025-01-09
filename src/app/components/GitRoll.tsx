cc871cfd0fe8a53e08fcaf7538787a16e6f5e4bc
Devansh Baghel <devanshbaghel85@gmail.com>
2025-01-09 17:43:33 +0530

    fix: gitroll image

--- src/app/components/GitRoll.tsx
+++ src/app/components/GitRoll.tsx
@@ -11,7 +11,7 @@ export default function GitRoll() {
         target="_blank"
       >
         <img
-          src="https://gitroll.io/api/badges/profiles/v1/uEYBeQmpK8LQdxyyFmiTOicIEPrG2?theme=light"
+          src="gitroll-normal.png"
           className="wrapper w-full rounded-[30px] border-[3px] border-slate-900 shadow-[4px_4px_0px_0px_#1e293b] transition-all duration-200 hover:translate-x-1 hover:translate-y-1 hover:shadow-none"
           alt="GitRoll Profile Badge"
         />

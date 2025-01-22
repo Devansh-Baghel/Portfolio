b1e25545e4d7e4d961fd58fb63a73b4aa943c8c0
Devansh Baghel <devanshbaghel85@gmail.com>
2025-01-22 07:22:11 +0530

    fix: animations doesn't ruin hover button effect

--- src/app/components/Stack.tsx
+++ src/app/components/Stack.tsx
@@ -47,71 +47,79 @@ export default function TechStack() {
         <div className="stack-wrapper flex grid-cols-[2.2fr_0.9fr] grid-rows-[1fr_1fr] flex-col gap-6 md:grid">
           <div
             data-aos="fade-right"
-            className="wrapper col-start-1 col-end-2 row-start-1 row-end-2 rounded-[30px] border-[3px] border-slate-900 p-6 shadow-[4px_4px_0px_0px_#1e293b] transition-all duration-200 hover:translate-x-1 hover:translate-y-1 hover:shadow-none"
+            className="col-start-1 col-end-2 row-start-1 row-end-2"
           >
-            <h3 className="vertical mb-4 text-3xl">Frontend</h3>
+            <div className="wrapper rounded-[30px] border-[3px] border-slate-900 p-6 shadow-[4px_4px_0px_0px_#1e293b] transition-all duration-200 hover:translate-x-1 hover:translate-y-1 hover:shadow-none">
+              <h3 className="vertical mb-4 text-3xl">Frontend</h3>
 
-            <div className="flex max-w-80 flex-wrap gap-4">
-              {frontend.map((elm) => (
-                <span
-                  className="hint--top hint--rounded"
-                  key={elm.path}
-                  aria-label={elm.name}
-                >
-                  <Image
-                    height={45}
-                    width={45}
-                    alt={elm.name}
-                    src={`/skillicons/${elm.path}`}
-                  />
-                </span>
-              ))}
+              <div className="flex max-w-80 flex-wrap gap-4">
+                {frontend.map((elm) => (
+                  <span
+                    className="hint--top hint--rounded"
+                    key={elm.path}
+                    aria-label={elm.name}
+                  >
+                    <Image
+                      height={45}
+                      width={45}
+                      alt={elm.name}
+                      src={`/skillicons/${elm.path}`}
+                    />
+                  </span>
+                ))}
+              </div>
             </div>
           </div>
+
           <div
             data-aos="fade-right"
-            className="wrapper col-start-1 col-end-2 row-start-2 row-end-3 rounded-[30px] border-[3px] border-slate-900 p-6 shadow-[4px_4px_0px_0px_#1e293b] transition-all duration-200 hover:translate-x-1 hover:translate-y-1 hover:shadow-none"
+            className="col-start-1 col-end-2 row-start-2 row-end-3"
           >
-            <h3 className="vertical mb-4 text-3xl">Backend</h3>
+            <div className="wrapper rounded-[30px] border-[3px] border-slate-900 p-6 shadow-[4px_4px_0px_0px_#1e293b] transition-all duration-200 hover:translate-x-1 hover:translate-y-1 hover:shadow-none">
+              <h3 className="vertical mb-4 text-3xl">Backend</h3>
 
-            <div className="flex max-w-80 flex-wrap gap-4">
-              {backend.map((elm) => (
-                <span
-                  className="hint--top hint--rounded"
-                  key={elm.path}
-                  aria-label={elm.name}
-                >
-                  <Image
-                    height={45}
-                    width={45}
-                    alt={elm.name}
-                    src={`/skillicons/${elm.path}`}
-                  />
-                </span>
-              ))}
+              <div className="flex max-w-80 flex-wrap gap-4">
+                {backend.map((elm) => (
+                  <span
+                    className="hint--top hint--rounded"
+                    key={elm.path}
+                    aria-label={elm.name}
+                  >
+                    <Image
+                      height={45}
+                      width={45}
+                      alt={elm.name}
+                      src={`/skillicons/${elm.path}`}
+                    />
+                  </span>
+                ))}
+              </div>
             </div>
           </div>
+
           <div
             data-aos="fade-up"
-            className="wrapper col-start-2 col-end-3 row-start-1 row-end-3 rounded-[30px] border-[3px] border-slate-900 p-6 shadow-[4px_4px_0px_0px_#1e293b] transition-all duration-200 hover:translate-x-1 hover:translate-y-1 hover:shadow-none"
+            className="col-start-2 col-end-3 row-start-1 row-end-3 md:h-full"
           >
-            <h3 className="vertical mb-4 text-3xl">Misc.</h3>
+            <div className="wrapper rounded-[30px] border-[3px] border-slate-900 p-6 shadow-[4px_4px_0px_0px_#1e293b] transition-all duration-200 hover:translate-x-1 hover:translate-y-1 hover:shadow-none md:h-full">
+              <h3 className="vertical mb-4 text-3xl">Misc.</h3>
 
-            <div className="flex max-w-80 flex-wrap gap-4">
-              {misc.map((elm) => (
-                <span
-                  className="hint--top hint--rounded"
-                  key={elm.path}
-                  aria-label={elm.name}
-                >
-                  <Image
-                    height={45}
-                    width={45}
-                    alt={elm.name}
-                    src={`/skillicons/${elm.path}`}
-                  />
-                </span>
-              ))}
+              <div className="flex max-w-80 flex-wrap gap-4">
+                {misc.map((elm) => (
+                  <span
+                    className="hint--top hint--rounded"
+                    key={elm.path}
+                    aria-label={elm.name}
+                  >
+                    <Image
+                      height={45}
+                      width={45}
+                      alt={elm.name}
+                      src={`/skillicons/${elm.path}`}
+                    />
+                  </span>
+                ))}
+              </div>
             </div>
           </div>
         </div>

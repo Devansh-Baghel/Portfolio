1c6653c92536b2b4f7303a1062cc97336c0c30a7
Devansh Baghel <devanshbaghel85@gmail.com>
2026-01-18 21:44:19 +0530

    feat: footer placement

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,12 +4,12 @@ export default function Footer() {
   const currentYear = new Date().getFullYear()
 
   return (
-    <footer className="relative z-10 mt-24 p-6 pb-20 text-slate-900">
+    <footer className="relative z-10 mt-24 p-6 pb-20 text-slate-600">
       <div className="p-6 text-center">
-        <p className="text-lg flex gap-1.5 items-center justify-center">
-          Made with <HandHeartIcon className="size-7" /> and <span className="text-lime-500">not ai</span> by yours truly.
-        </p>
-        <p className="mt-2 text-sm text-slate-600">
+        <span className="text-lg flex gap-1.5 items-center justify-center">
+          Made with <HandHeartIcon className="size-7" /> by yours truly.
+        </span>
+        <p className="mt-2 text-sm text-slate-400">
           © {currentYear} Devansh Baghel
         </p>
       </div>
